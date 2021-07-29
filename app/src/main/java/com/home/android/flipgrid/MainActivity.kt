a44c41b145fab4d39eb6ae3fe96f2f881804a68d
Sujit Singh <sujitsingh@Sujits-MacBook-Pro.local>
2021-07-29 16:38:16 -0400

    initial layout of register fragment and Fragment structure

--- app/src/main/java/com/home/android/flipgrid/MainActivity.kt
+++ app/src/main/java/com/home/android/flipgrid/MainActivity.kt
@@ -2,9 +2,15 @@ package com.home.android.flipgrid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commitNow
+import com.home.android.flipgrid.register.RegisterFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportFragmentManager.commitNow(true) {
+            setReorderingAllowed(true)
+            add(R.id.fragment_container_view, RegisterFragment.newInstance())
+        }
     }
 }
