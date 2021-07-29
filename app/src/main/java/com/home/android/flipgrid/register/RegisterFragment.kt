924327fc96b4c1aa075c6025253eecc455f566d5
Sujit Singh <sujitsingh@Sujits-MacBook-Pro.local>
2021-07-29 16:24:06 -0400

    Create basic template

---
+++ app/src/main/java/com/home/android/flipgrid/register/RegisterFragment.kt
@@ -0,0 +1,15 @@
+package com.home.android.flipgrid.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.home.android.flipgrid.R
+
+
+class RegisterFragment : Fragment(R.layout.fragment_register) {
+
+    companion object {
+    }
+}
